45ddcc6c8c3abf66de7ccfb493e45314c69ec111
Emma Karayiannis <emma.karayiannis@gmail.com>
2020-08-03 09:54:04 +0100

    Commit add/remove books

--- library/library.js
+++ library/library.js
@@ -1,5 +1,11 @@
 let myLibrary = [];
 
+const bookshelf = document.querySelector('.bookshelf');
+const addButton = document.querySelector('.add-book');
+const modal = document.querySelector('.modal');
+const closeButton = modal.querySelector('.close');
+const submitButton = modal.querySelector('.submit');
+
 function Book(title, author, pages, read) {
   this.title = title,
   this.author = author,
@@ -16,35 +22,77 @@ addBookToLibrary('The Hobbit', 'J.R.R Tolkien', 295, 'not read');
 addBookToLibrary('Northern Lights', 'Philip Pullman', 399, 'read');
 addBookToLibrary('The Handmaid\'s Tale', 'Margaret Attwood', 311, 'not read');
 
-function render(myLibrary) {
-
-  const container = document.querySelector('.bookshelf');
-
-  for (let index = 0; index < myLibrary.length; index++) {
-
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.setAttribute('data-index', `card-${index}`);
-
-    const title = document.createElement('h2');
-    title.textContent = myLibrary[index].title;
-    const author = document.createElement('p');
-    author.textContent = `By ${myLibrary[index].author}`;
-    const pages = document.createElement('p');
-    pages.textContent = `Pages: ${myLibrary[index].pages}`;
-    const read = document.createElement('p');
-    read.textContent = myLibrary[index].read;
-    card.append(title, author, pages, read);
-
-    container.append(card);
-  }
+function processArray(element, index) {
+  const title = element.title;
+  const author = element.author;
+  const pages = element.pages;
+  const read = element.read;
+  const libraryIndex = index;
+  renderNewBook(title, author, pages, read, libraryIndex);
 }
+
+
+function render(myLibrary) {
+  myLibrary.forEach(processArray);
+}
+
+//
+
+function renderNewBook(title, author, pages, read, index) {
+  const book = document.createElement('div');
+  book.classList.add('card');
+  book.setAttribute('data-index', `${index}`);
+  const button = document.createElement('button');
+  button.classList.add('close', 'card__close');
+  button.type = 'button';
+  button.innerHTML = 'x';
+  book.append(button);
+  button.addEventListener('click', () => {
+    button.closest('.card').remove();
+    myLibrary.splice(index, 1);
+  });
+
+  const bookTitle = document.createElement('h2');
+  bookTitle.textContent = title;
+  const bookAuthor = document.createElement('p');
+  bookAuthor.textContent = `By ${author}`;
+  const bookPages = document.createElement('p');
+  bookPages.textContent = `Pages: ${pages}`;
+  const bookRead = document.createElement('p');
+  bookRead.textContent = read;
+  book.append(bookTitle, bookAuthor, bookPages, bookRead);
+  bookshelf.append(book);
+  // cardCloseButtons();
+}
+
+function submitBook(event) {
+  event.preventDefault();
+  const form = document.querySelector('.book-form');
+  const title = document.getElementById('title').value;
+  const author = document.getElementById('author').value;
+  const pages = document.getElementById('pages').value;
+  let read =  document.getElementById('read_state').value;
+
+  if (read === 'on') {
+    read = 'Read';
+  } else {
+    read = 'Unread';
+  }
+
+  form.reset();
+  modal.classList.remove('modal--visible');
+  addBookToLibrary(title, author, pages, read);
+  const index = myLibrary.length - 1;
+  renderNewBook(title, author, pages, read, index);
+}
+
+function toggleRead() {
+
+}
+
 
 render(myLibrary);
 
-const addButton = document.querySelector('.add-book');
-const modal = document.querySelector('.modal');
-const closeButton = modal.querySelector('.close');
-
 addButton.addEventListener('click', () => modal.classList.add('modal--visible'));
 closeButton.addEventListener('click', () => modal.classList.remove('modal--visible'));
+submitButton.addEventListener('click', submitBook);
